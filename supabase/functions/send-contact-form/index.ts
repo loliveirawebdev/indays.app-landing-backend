8d8bd47fcb583f7aa012a05ab81ade463e1ddb05
Lucas Oliveira <lucas.oliveira@simbioseventures.com>
2022-12-29 14:12:22 -0300

    feat: add cors headers

--- supabase/functions/send-contact-form/index.ts
+++ supabase/functions/send-contact-form/index.ts
@@ -2,10 +2,20 @@ import { parser } from "./parser.ts";
 import { serve } from "https://deno.land/std@0.131.0/http/server.ts";
 import { sendMail } from "https://deno.land/x/sendgrid@0.0.3/mod.ts";
 
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers":
+    "authorization, x-client-info, apikey, content-type",
+};
+
 const name = "Lucas Oliveira";
 const email = "lucas.oliveira@simbioseventures.com";
 
 serve(async (req: Request) => {
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
+  }
+
   const formData = await req.json();
   const parsedData = parser(formData);
   const sendgridApiKey = Deno.env.get("SENDGRID_API_KEY");
@@ -26,6 +36,9 @@ serve(async (req: Request) => {
 
   return new Response(
     null,
-    { status: 200 },
+    {
+      status: 200,
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+    },
   );
 });
